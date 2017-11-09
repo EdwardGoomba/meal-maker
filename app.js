496bfb916c2d5f63519300116344135ac6f9056f
EdwardGoomba <ask@edwardvdesign.com>
2017-11-09 16:38:23 -0500

    Added get courses and add dish to course method

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 //Code looks through menu and makes meal combinations for you
 
 //Main menu setup
-let menu = {
+const menu = {
   _courses: {
     _appetizers: [],
     _mains: [],
     _desserts: []
+  },
+
+  addDishToCourse(courseName, dishName, dishPrice) {
+
+  },
+
+  get courses() {
+    return {
+      appetizers: this._courses.appetizers,
+      mains: this._courses.mains,
+      desserts: this._courses.desserts
+    }
   },
 
   get appetizers() {
